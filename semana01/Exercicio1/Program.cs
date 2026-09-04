178f15f022621996e5753183ac3ff5537db60050
LohanBrito <lohanbrito97@gmail.com>
2026-09-04 20:26:15 -0300

    Exercicio 1

--- semana01/Exercicio1/Program.cs
+++ semana01/Exercicio1/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio1.");
+
+        Console.Write("Qual é o seu nome? ");
+        string nome = Console.ReadLine();
+        
+        Console.Write("Qual é seu sobrenome? ");
+        string sobrenome = Console.ReadLine();
+        Console.WriteLine($"Seu nome é {sobrenome}, {nome} {sobrenome}.");
     }
 }
