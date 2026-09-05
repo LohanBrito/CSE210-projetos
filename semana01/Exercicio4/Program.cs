dacd3f80f3d0ccf8232f86c49609b7a8468062c0
LohanBrito <lohanbrito97@gmail.com>
2026-09-05 16:05:58 -0300

    Exercicios feitos

--- semana01/Exercicio4/Program.cs
+++ semana01/Exercicio4/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio4.");
+        
+        
+        List<int> lista = new List<int>();
+        int numero;
+
+        do
+        {
+            Console.Write("Insira uma lista de números e digite 0 quando terminar: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero != 0)
+            {
+                lista.Add(numero);
+                Console.WriteLine("Você digitou o número: " + numero);
+            }   
+
+        }
+        
+        while (numero != 0);
+        {
+            if (lista.Count > 0)
+            {
+                int soma = lista.Sum();
+                double media = lista.Average();
+                int maior = lista.Max();
+                int total = lista.Count;
+
+                var ordenada = lista.OrderBy(x => x).ToList();
+
+                Console.WriteLine("Soma: " + soma);
+                Console.WriteLine("Média: " + media);
+                Console.WriteLine("O maior número é: " + maior);
+                Console.WriteLine("Total de itens: " + total);
+                Console.WriteLine("Lista ordenada: " + string.Join(", ", ordenada));   
+            }
+
+            else
+            {
+              Console.WriteLine("Nenhum número digitado");  
+            }
+        }  
     }
 }
--- semana01/Exercicio5/Program.cs
+++ semana01/Exercicio5/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio5.");
+        string nome = PerguntarNomeUsuario();
+        int numero = PerguntarNumeroFavorito();
+        int resultado = ElevarAoQuadrado(numero);
+        
+        Console.WriteLine($"Olá, {nome}! Seu número favorito é: {numero}");
+        Console.WriteLine($"Irmão {nome}, o quadrado do seu número é: {resultado}.");
     }
+
+    static void ExibirBoasVindas()
+    {
+        Console.WriteLine("Bem-Vindo ao Programa");
+    }
+
+    static string PerguntarNomeUsuario()
+    {
+        Console.Write("Qual é seu nome: ");
+        string nome = Console.ReadLine();
+        return nome;
+    }
+
+    static int PerguntarNumeroFavorito()
+    {
+        Console.Write($"Qual é seu número favorito? ");
+        int numero = int.Parse(Console.ReadLine());
+        return numero;
+    }
+
+    static int ElevarAoQuadrado( int num)
+    {
+        return num * num;
+    }
+
 }
