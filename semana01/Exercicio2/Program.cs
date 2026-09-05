4740eb487e9bc9ab08862a25d758c9872d903c0c
LohanBrito <lohanbrito97@gmail.com>
2026-09-04 22:19:08 -0300

    Exercicios feitos

--- semana01/Exercicio2/Program.cs
+++ semana01/Exercicio2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio2.");
+        Console.Write("Qual o percentual da sua nota? ");
+        string resposta = Console.ReadLine();
+        int nota = int.Parse(resposta);
+
+        string letra = "";
+
+        if (nota >= 90)
+        {
+            letra = "A";
+        }
+        else if (nota >= 80)
+        {
+            letra = "B";
+        }
+        else if (nota >= 70)
+        {
+            letra = "C";
+        }
+        else if (nota >= 60)
+        {
+            letra = "D";
+        }
+        else
+        {
+            letra = "F";
+        }
+        
+        Console.WriteLine($"Sua nota é: {letra}");
+
+        if (nota >= 70)
+        {
+            Console.WriteLine("Parabéns! Você passou!");
+        }
+        else
+        {
+            Console.WriteLine("Não foi dessa vez. Tente Novamente!");
+        }
     }
 }
--- semana01/Exercicio3/Program.cs
+++ semana01/Exercicio3/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio3.");
+        //Foi utilizado para digitar na primeira parte do exercicio.
+        //Console.Write("Qual é seu número mágico? ");
+        //string numMagico = Console.ReadLine();
+        //int numero = int.Parse(numMagico);
+
+        //Feita a parte com numero Randon;
+        //Acrescentados os desafios adicionais.
+
+        string jogarNovamente = "sim";
+
+        while (jogarNovamente.ToLower() == "sim")
+        {
+            Random geradorRandom = new Random();
+            int numero = geradorRandom.Next(1, 101);
+
+            int palpite = -1;
+            int contadorPalpite = 0;
+
+            Console.WriteLine("Tente adivinhar o número de 1 a 100!");
+
+            while (palpite != numero)
+            {
+                Console.Write("Qual é seu palpite? ");
+                string numPalpite = Console.ReadLine();
+                palpite = int.Parse(numPalpite);
+                contadorPalpite++;
+            
+                if (palpite < numero)
+                {
+                    Console.WriteLine("Mais Alto");
+                }
+
+                else if (palpite > numero)
+                {
+                    Console.WriteLine("Mais baixo");
+                }
+
+                else
+                {
+                    Console.WriteLine("Você Adivinhou! Parabéns");
+                    Console.WriteLine($"Você fez {contadorPalpite} palpites nessa jogada.");
+                }  
+            }
+            Console.Write("Deseja Jogar novamente? (sim/não): ");
+            jogarNovamente = Console.ReadLine();  
+        }
+        Console.WriteLine("Obrigado por jogar!");              
     }
 }
