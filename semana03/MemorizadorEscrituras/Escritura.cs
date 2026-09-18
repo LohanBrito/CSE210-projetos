2193d07082e8e1d3f7cad2f12b4ae39df388fb12
LohanBrito <lohanbrito97@gmail.com>
2026-09-18 14:29:29 -0300

    Finalizações

--- semana03/MemorizadorEscrituras/Escritura.cs
+++ semana03/MemorizadorEscrituras/Escritura.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 
 public class Escritura
@@ -9,10 +10,10 @@ public class Escritura
     private List<Palavra> _palavra;
 
 
-    public void Escritura(Referencia referencia, string texto)
+    public Escritura(Referencia referencia, string texto)
     {
         _referencia = referencia;
-        _palavra = new List<Palavra>;
+        _palavra = new List<Palavra>();
 
         // Separar o texto em palavras:
         string[] palavrasTexto = texto.Split(' ');
@@ -22,11 +23,48 @@ public class Escritura
         }
     }
 
+    //Será feito um filtro das palavras que não foram ocultadas.
+    public void OcultarPalavrasAleatorias(int numeroParaOcultar)
+    {
+        Random random = new Random();
 
+        List<Palavra> palavrasVisiveis = _palavra.Where(p => !p.EstaOculta()).ToList();
 
+        for (int i = 0; i < quantidadeAOcultar; i++)
+        {
+            int indiceAleatorio = random.Next(palavrasVisiveis.Count);
+            palavrasVisiveis[indiceAleatorio].Ocultar();
 
+            // Remove da lista a palavra já ocultada no loop.
+            palavrasVisiveis.RemoveAt(indiceAleatorio);
+        }
+    }
 
+        public string ObterTexto()
+        {
+            List<string> textosPalavras = new List<string>();
 
+            foreach (Palavra palavra in _palavra)
+            {
+                textosPalavras.Add(palavra.ObterTexto());
+            }
 
+            string textoCompleto = string.Join(" ", textosPalavras);
 
+            return $"{_referencia.ObterTexto()} - {textoCompleto}";
+        }
+
+        public bool EstaCompletamenteOculta()
+        {
+            foreach (Palavra palavra in _palavra)
+            {
+                if(!palavra.EstaOculta())
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
 }
